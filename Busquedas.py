fba5a7e314eac243a935fc37dc8a3a74203ed184
DaniiVCGamer <daniel.vargas@correounivalle.edu.co>
2022-02-07 17:08:02 -0500

    ijofhrygrrrgrtrh

--- Busquedas.py
+++ Busquedas.py
@@ -61,7 +61,7 @@ def principal():
         for i in range(len(lista_csvs)):
             cont = 0
             with open(lista_csvs[i]) as p:
-                print(lista_csvs[i])
+                print("XXXXXXXXXXXXXXXXXXXXXXXXXXXX:  ", lista_csvs[i])
                 reader = csv.reader(p, delimiter=';')
                 for row in reader:
                     for j in range(len(text)):
